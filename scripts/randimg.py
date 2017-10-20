ce90434bf786b5e2c1f0ad2c5a93852361d15d49
epicdaftman <williamjrmoody@icloud.com>
2017-10-20 17:23:37 -0400

    Switched random to be wallhaven instead of imgur

--- scripts/randimg.py
+++ scripts/randimg.py
@@ -1,18 +1,6 @@
-#Made by daft https://github.com/epicdaftman
 import requests
 
-def main():
-  img = getImg()
-  print(img)
-  while img[::-1][0:4] != 'gpj.': #Test to see if the image is a jpg.
-    print(img)
-    img = getImg()
-  r = requests.get('https://'+img)
-  open('/home/daft/.config/i3/wall','wb+').write((r.content))
-
-
-def getImg():
-  r = requests.get('https://imgur.com/random') #Load the random image page
-  return r.text.split('src="')[3][2:].split('"')[0] #Magic
-    
-main() 
+r = requests.get('https://alpha.wallhaven.cc/random')
+num = r.text.split('href="')[29][0:100].split('/')[4].split('"')[0]
+r = requests.get('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-'+str(num)+'.jpg')
+open('../wall','wb+').write(r.content)
